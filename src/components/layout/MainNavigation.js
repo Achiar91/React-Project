8a8422a690a19b8403d6a9ccf8a3087f6ff4c4f1
Andrei Chiar <a.chiar@yahoo.com>
2021-08-18 14:09:12 +0300

    added the badge feature using again the useContext feature for the favorites page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css'
+import {useContext} from 'react';
+import FavoritesContext from '../../store/favorites-context';
 function MainNavigation() {
+    const favoritesCtx = useContext(FavoritesContext);
     return <header className={classes.header}>
         <div className={classes.logo}>React Meetups</div>
         <nav>
@@ -12,7 +15,8 @@ function MainNavigation() {
                     <Link to='/New-meetup'>Add New meetup</Link>
                 </li>
                 <li>
-                    <Link to='/Favorites'>My Favorites</Link>
+                    <Link to='/Favorites'>My Favorites
+                    <span className={classes.badge}>{favoritesCtx.totalFavorites}</span></Link>
                 </li>
             </ul>
         </nav>
