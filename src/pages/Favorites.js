c938046c859d6135777a7e1a47c86d5471c79e7f
Andrei Chiar <a.chiar@yahoo.com>
2021-08-18 14:03:06 +0300

    added content for the favorites page using the Meetup list that I created earlier

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,4 +1,21 @@
-function Favorites () {
-    return <div>Favorites page</div>
+import { useContext } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+import FavoritesContext from '../store/favorites-context';
+function Favorites() {
+    const favoriteCtx = useContext(FavoritesContext);
+    let content;
+    if (favoriteCtx.totalFavorites === 0) {
+        content = <p>You got no favorites yet!</p>
+    } else {
+        content = <MeetupList meetups={favoriteCtx.favorites} />
     }
-    export default Favorites;
+    return (
+    <section>
+        <h1>My Favorites</h1>
+
+        {content}
+
+    </section>
+    );
+}
+export default Favorites;
